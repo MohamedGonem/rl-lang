ee6e422aacdc0f91e589d3ba9b7d30fe10a1efe1
MohamedGonem <nightangelyt01@gmail.com>
2026-06-06 20:57:38 +0300

    add more std::math functions

---
+++ src/interpreter/stdlib/math/abs.rs
@@ -0,0 +1,15 @@
+use crate::{
+    interpreter::{evaluator::Evaluator, values::Value},
+    utils::errors::Error,
+};
+
+pub fn std_abs(_: &mut Evaluator, a: Value) -> Value {
+    match a {
+        Value::Integer(i) => Value::Integer(i.abs()),
+        Value::Float(f) => Value::Float(f.abs()),
+        _ => {
+            Error::init("".to_string(), None, None).print_error();
+            unreachable!()
+        }
+    }
+}
