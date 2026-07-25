fc7b9ca597e085761d443cb72494d9f1b03eeaee
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-25 09:47:35 +0300

    add `close` function to remove handles from `Evaluator`

--- crates/rl-interpreter/src/stdlib/c/mod.rs
+++ crates/rl-interpreter/src/stdlib/c/mod.rs
@@ -2,6 +2,7 @@ use crate::native::Module;
 use libloading::Library;
 
 mod call;
+mod close;
 mod common;
 mod compile;
 pub enum CHandle {
@@ -12,4 +13,5 @@ pub fn module() -> Module {
     Module::new("c")
         .with_function("compile", compile::func)
         .with_function("call", call::func)
+        .with_function("close", close::func)
 }
