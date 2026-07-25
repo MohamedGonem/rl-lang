df52d5d7f91c7180db2614e4db579c6bb235ec4c
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-25 09:28:46 +0300

    create helper function to construct the compilation command

--- crates/rl-interpreter/src/stdlib/c/compile.rs
+++ crates/rl-interpreter/src/stdlib/c/compile.rs
@@ -1,7 +1,25 @@
+use std::path::PathBuf;
+use std::process::Command;
+
 #[cfg(target_os = "windows")]
 const SHARED_LIB_EXT: &str = "dll";
 #[cfg(target_os = "macos")]
 const SHARED_LIB_EXT: &str = "dylib";
 #[cfg(not(any(target_os = "windows", target_os = "macos")))]
 const SHARED_LIB_EXT: &str = "so";
+
+fn compiler_command(compiler: &str, src: &PathBuf, out: &PathBuf) -> Command {
+    let mut cmd = Command::new(compiler);
+    #[cfg(target_os = "macos")]
+    {
+        cmd.args(["-dynamiclib", "-O2", "-o"]).arg(out).arg(src);
+    }
+    #[cfg(not(target_os = "macos"))]
+    {
+        cmd.args(["-shared", "-fPIC", "-O2", "-o"])
+            .arg(out)
+            .arg(src);
+    }
+    cmd
+}
 
